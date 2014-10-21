9f336b44dcf1a05c10e96a0e39c060974dc76e3a
Alejandro Machado <alemacgo@gmail.com>
2014-10-21 11:03:46 +0100

    Add photo picker

--- Prototype/TodayViewController.swift
+++ Prototype/TodayViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class TodayViewController: UIViewController, UIScrollViewDelegate {
+class TodayViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     let horizontalManager = HorizontalTransitionManager()
     let verticalManager = VerticalTransitionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
     }
     
     @IBAction func unwindTodayFromWishList(sender: UIStoryboardSegue) {
@@ -48,6 +50,80 @@ class TodayViewController: UIViewController, UIScrollViewDelegate {
                     toViewController.transitioningDelegate = self.horizontalManager
             }
         }
+    }
+    
+    // Taking photos
+    @IBOutlet weak var imageView: UIImageView!
+    let picker = UIImagePickerController()
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    func imagePickerController(picker: UIImagePickerController!,
+        didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!){
+            
+            println("Picker returned successfully")
+            
+            let mediaType:AnyObject? = info[UIImagePickerControllerMediaType]
+            
+            if let type:AnyObject = mediaType{
+                
+                if type is String{
+                    let stringType = type as String
+                    
+                    if stringType == kUTTypeImage as NSString {
+                        
+                        var theImage: UIImage!
+                        
+                        if picker.allowsEditing{
+                            theImage = info[UIImagePickerControllerEditedImage] as UIImage
+                        } else {
+                            theImage = info[UIImagePickerControllerOriginalImage] as UIImage
+                        }
+                        let width = theImage.size.width
+                        let height = theImage.size.height
+                        if (height != width) {
+                            var newDimension = min(width, height);
+                            var widthOffset = (width - newDimension) / 2;
+                            let heightOffset = (height - newDimension) / 2;
+                            UIGraphicsBeginImageContextWithOptions(CGSizeMake(newDimension, newDimension), false, 0.0);
+                            theImage.drawAtPoint(CGPointMake(-widthOffset, -heightOffset),
+                                blendMode:kCGBlendModeCopy,
+                                alpha:1.0)
+                            theImage = UIGraphicsGetImageFromCurrentImageContext();
+                            UIGraphicsEndImageContext();
+                        }
+                        imageView.image = theImage
+                        cameraButton.hidden = true
+                    }
+                    
+                }
+            }
+            
+            picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    @IBAction func didTapCameraButton(sender: UIButton) {
+        picker.sourceType = .Camera
+        
+        var f = picker.view.bounds
+        f.size.height -= picker.navigationBar.bounds.size.height
+        let barHeight = f.size.height/2 - f.size.width/2
+        UIGraphicsBeginImageContext(f.size)
+        //UIColor.blackColor().colorWithAlphaComponent(0.5).set()
+        UIRectFillUsingBlendMode(CGRectMake(0, 0, f.size.width, barHeight), kCGBlendModeNormal);
+        UIRectFillUsingBlendMode(CGRectMake(0, f.size.height - barHeight, f.size.width, 0.75*barHeight), kCGBlendModeNormal);
+        let overlayImage = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        let overlayIV = UIImageView(frame: f)
+        overlayIV.image = overlayImage;
+        //overlayIV.userInteractionEnabled = false
+        picker.cameraOverlayView?.userInteractionEnabled = false
+        picker.cameraOverlayView?.addSubview(overlayIV)
+        
+        self.presentViewController(picker, animated: true, completion: nil)
     }
 }
 
