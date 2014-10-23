21c93ec8a9d5582ddd24954909f7ed360a13508b
Alejandro Machado <alemacgo@gmail.com>
2014-10-23 20:21:03 +0100

    Add new profile screens

--- Prototype/ProfileViewController.swift
+++ Prototype/ProfileViewController.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+let badgesImage = UIImage(named: "badges")
+let logImage = UIImage(named: "profileLog")
+
 class ProfileViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
@@ -15,5 +18,16 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = scrollView.subviews[0].size!
+        scrollView.contentOffset.y = 140
+    }
+    
+    @IBAction func didTapTrophy(sender: UIButton) {
+        (scrollView.subviews[0] as UIImageView).image = badgesImage
+        scrollView.contentOffset.y = 0
+    }
+    
+    @IBAction func didTapBook(sender: UIButton) {
+        (scrollView.subviews[0] as UIImageView).image = logImage
+        scrollView.contentOffset.y = 140
     }
 }
