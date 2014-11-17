c1c99421debf37f8264e44549d202536eac01445
Alejandro Machado <alemacgo@gmail.com>
2014-11-17 16:27:46 -0500

    Add map view

--- Prototype/FeedViewController.swift
+++ Prototype/FeedViewController.swift
@@ -28,24 +28,38 @@ enum BarX: CGFloat {
     case World = 192
 }
 
+enum FeedCenter: CGFloat {
+    case Original = 352.5
+    case Low = 534.5 // Original + mapView.frame.size.height
+}
+
+let LisbonCenter = CGPoint(x: 1141.5, y: 446)
+
 let clockAscending = UIImage(named: "ascending")
 let clockDescending = UIImage(named: "descending")
 
 let smellAscending = UIImage(named: "smellAscending")
 let smellDescending = UIImage(named: "smellDescending")
+let smellEast = UIImage(named: "smellEast")
+let smellWest = UIImage(named: "smellWest")
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UIScrollViewDelegate {
   
     var displayMode: DisplayMode!
     var challenge: Challenge!
     
     @IBOutlet weak var feedView: UIScrollView!
+    @IBOutlet weak var mapView: UIScrollView!
+    
     @IBOutlet weak var clockIcon: UIImageView!
     @IBOutlet weak var feedImage: UIImageView!
     @IBOutlet weak var selectedModeBar: UIImageView!
     
     override func viewDidLoad() {
         feedView.contentSize = feedView.subviews[0].size!
+        mapView.contentSize = mapView.subviews[0].size!
+        mapView.contentOffset = LisbonCenter
+
         challenge = .Smell
         displayMode = .Descending
         updateFeed()
@@ -72,6 +86,10 @@ class FeedViewController: UIViewController {
             UIView.animateWithDuration(0.3) {
                 self.selectedModeBar.center.x = BarX.Clock.rawValue
             }
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.8, options: nil, animations: {
+                self.mapView.layer.opacity = 0
+                self.feedView.center.y = FeedCenter.Original.rawValue
+                }, completion: nil)
         }
         else if displayMode == .Ascending {
             clockIcon.image = clockAscending
@@ -80,6 +98,10 @@ class FeedViewController: UIViewController {
             UIView.animateWithDuration(0.3) {
                 self.selectedModeBar.center.x = BarX.World.rawValue
             }
+            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.8, options: nil, animations: {
+                self.mapView.layer.opacity = 1
+                self.feedView.center.y = FeedCenter.Low.rawValue
+            }, completion: nil)
         }
         
         if challenge == .Smell {
@@ -88,6 +110,23 @@ class FeedViewController: UIViewController {
             }
             else if displayMode == .Ascending {
                 feedImage.image = smellAscending
+            }
+            else if displayMode == .Map {
+                feedImage.image = smellEast
+            }
+        }
+        feedView.contentOffset.y = 0 // Bring the feed to the top image
+    }
+    
+    // MARK: Scroll View Custom Behaviors
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        if scrollView == feedView {
+            if scrollView.center.y != FeedCenter.Original.rawValue {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.8, options: nil,
+                    animations: {
+                        self.mapView.layer.opacity = 0
+                        scrollView.center.y = FeedCenter.Original.rawValue
+                    }, nil)
             }
         }
     }
